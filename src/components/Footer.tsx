3b5716d3e283c1684cdc3b604a0e5f50805824e9
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-07-31 16:01:39 +0800

    Configure styles, adding Head info, move files around.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <div className="attribution fixed bottom-0 left-[50%] w-full translate-x-[-50%] p-2">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>{" "}
+      Coded by <a href="#">Pakkerman</a>
+    </div>
+  )
+}
+
+export default Footer
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
-import { type Config } from "tailwindcss";
+import { type Config } from "tailwindcss"
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        White: "hsl(0, 0%, 100%)",
+        LightGrayishViolet: "hsl(270, 3%, 87%)",
+        DarkGrayishViolet: "hsl(279, 6%, 55%)",
+        VeryDarkViolet: "hsl(278, 68%, 11%)",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
