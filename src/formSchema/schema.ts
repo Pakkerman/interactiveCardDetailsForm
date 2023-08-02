fdb151997b933e344b59dc82314e5ef3f3b23680
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-08-02 23:12:28 +0800

    Update validation schema

--- src/formSchema/schema.ts
+++ src/formSchema/schema.ts
@@ -3,31 +3,31 @@ import * as yup from "yup"
 export const formSchema = yup.object().shape({
   holderName: yup
     .string()
-    .matches(/^[A-z\s-]+$/, "Invalid")
-    .max(30)
+    .matches(/^[A-z\s-]+$/, "Must only contain alphabet letters")
+    .max(30, "Exceed character limit")
     .required("Required"),
   cardNumber: yup
     .string()
-    .matches(/^[0-9]+/, "Invalid")
-    .length(16, "Invalid")
+    .matches(/^[0-9]+/, "Must only contain digits")
+    .length(16, "Must contain 16 digits")
     .required("Required"),
   month: yup
     .number()
     .typeError("Invalid")
-    .min(0, "Invalid")
-    .max(12, "Invalid")
-    .positive()
+    .min(0, "Invalid month")
+    .max(12, "Invalid month")
+    .positive("No negative")
     .integer()
     .required("Required"),
   year: yup
     .number()
     .typeError("Invalid")
-    .min(0, "Invalid")
-    .max(99, "Invalid")
-    .positive("Invalid")
+    .min(0, "Invalid year")
+    .max(99, "Invalid year")
+    .positive("No negative")
     .integer()
     .required("Required"),
-  CVC: yup.string().length(3, "Invalid").required("Required"),
+  CVC: yup.string().length(3, "Must contain 3 digits").required("Required"),
 })
 
 export type FormFields = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home() {
       year: "",
       CVC: "",
     },
+
     validationSchema: formSchema,
     onSubmit: () => {
       void mockAPI().then(() => {
@@ -44,10 +45,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className="flex flex-col items-center bg-slate-800 ">
-        <div className="flex w-full flex-col items-center bg-White md:flex-row ">
+        <div className="flex w-full flex-col items-center bg-White md:flex-row">
           <section className="relative h-[330px] md:h-full md:max-w-[483px] md:flex-[1_1] ">
             <div className="h-[240px] w-[100vw] bg-mobile md:hidden" />
-            <div className="hidden h-[100vh] max-h-[900px] min-h-[500px] bg-desktop md:block" />
+            <div className="hidden h-[100vh] max-h-[900px] min-h-[500px] bg-desktop transition-all md:block" />
             <div className="absolute left-[55%] top-[-20px] translate-x-[-45%] scale-[60%] transition-all md:left-[50%] md:top-[45%] md:translate-x-[-50%] md:scale-[65%] xl:left-[40%] xl:top-[50%] xl:translate-x-0 xl:scale-90">
               <CardBack CVC={formik.values.CVC} />
             </div>
